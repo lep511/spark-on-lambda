14b04d404986dcb88c31b48d9b221cd6981b9d40
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2024-08-24 16:05:59 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,10 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.types import *
-from pyspark.sql.functions import current_timestamp
-from datetime import datetime, date
 import json
 import sys
-import os
+import spark_script
       
 search_path = sys.path
 print(search_path)
 
-spark = SparkSession.builder \
-    .appName("Spark-on-AWS-Lambda") \
-    .master("local[*]") \
-    .config("spark.driver.bindAddress", "127.0.0.1") \
-    .config("spark.driver.memory", "5g") \
-    .config("spark.executor.memory", "5g") \
-    .enableHiveSupport().getOrCreate()
-
 def lambda_handler(event, context):
-    df = spark.createDataFrame([
-        Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
-        Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
-        Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
-    ])
-    print(df)
-    return {"body": json.dumps(str(df.take(1)))}
+    spark_script.main()
+    return {"body": "ok"}
