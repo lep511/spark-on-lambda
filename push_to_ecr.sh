104b03aec23eec550814cda4e183e9f0f73bd3c8
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2024-08-24 15:24:23 -0300

    Update push_to_ecr.sh

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -2,9 +2,19 @@
 
 #Script used to push the image to ECR repository. Please ensure that the role associated with the instance has access to AWS ECR
 
+# Check if AWS CLI is installed
+aws --version > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  # Install AWS CLI
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+  aws configure
+else
+  echo "AWS CLI is already installed"
+fi
+
 echo "Starting the PUSH to AWS ECR...."
-
-
 
 if [ $# -eq 0 ]
   then
@@ -12,6 +22,7 @@ if [ $# -eq 0 ]
 fi
 
 Dockerimage=$1
+FRAMEWORK=$2
 Dockername="spark-on-lambda"
 
 # Fetch the AWS account number
@@ -21,7 +32,6 @@ if [ $? -ne 0 ]
 then
     exit 255
 fi
-
 
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
 aws_region=$(aws configure get region)
@@ -42,7 +52,7 @@ aws ecr get-login-password --region "${aws_region}" | docker login --username AW
 
 # Build the docker image and push to ECR
 echo "Building the docker image"
-docker build  -t ${Dockerimage} .
+docker build --build-arg FRAMEWORK=$FRAMEWORK -t ${Dockerimage} .
 
 
 echo "Tagging the Docker image"
